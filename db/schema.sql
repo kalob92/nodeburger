791d1a4c599734f3080a6afaed451563efeb9fab
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-08-22 21:36:37 -0600

    .

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
-
 CREATE TABLE burgers (
 	id int AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
